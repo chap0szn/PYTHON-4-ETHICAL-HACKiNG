0612fcb84948f7d7ee901367a9a3c714cd6a1862
chap0szn <okpalajidestiny@gmail.com>
2025-04-03 12:16:50 +0100

    new push

---
+++ class-demo/hello.py
@@ -0,0 +1,17 @@
+# this line i running python without a variable.
+print('hello world')
+print("i am learning python programming language")
+
+# Declaring a variable
+f_name = "Destiny"
+l_name = "Okpalaji"
+
+
+# Concatenating a variable
+print(f_name + " " + l_name)
+
+age = 18
+sentence = "is the legal age age to vote in "
+country = "Nigeria"
+
+print(str(age) + " " + sentence + " " + country)
